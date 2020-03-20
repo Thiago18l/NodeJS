d0fa0c77bd57bb31efc2aef190fde2dc07f2620b
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-03-19 23:56:57 -0300

    Atualizando Middlewares

--- src/Express/src/Middleware.ts
+++ src/Express/src/Middleware.ts
@@ -11,6 +11,12 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     console.log("In another middleware");
+    next();
 });
+
+app.use((req, res, next) => {
+    console.log("Last Middleware");
+    res.send("The last Middleware...");
+})
 
 app.listen(PORT, () => console.log(`running app on port ${PORT}`))
