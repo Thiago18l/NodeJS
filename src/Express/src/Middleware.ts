a6773ee284e45b7b30dd049b8f3edd3b634e1c60
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-03-19 23:52:07 -0300

    Usando  Middlewares no express.js

--- src/Express/src/Middleware.ts
+++ src/Express/src/Middleware.ts
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
     console.log('Im the middleware');
-})
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("In another middleware");
+});
 
 app.listen(PORT, () => console.log(`running app on port ${PORT}`))
