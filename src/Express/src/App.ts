f0eb6c8f694f5cb06100983771c992bca92c9d80
Thiago Lopes <thiago.lopes.dev@gmail.com>
2020-03-22 02:09:02 -0300

    Adicionando código de erro para caminhos não registrados nas rotas

--- src/Express/src/App.ts
+++ src/Express/src/App.ts
@@ -11,6 +11,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(AdminRoutes);
 app.use(ShopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1> Page not found </h1>');
+})
+
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
 })
